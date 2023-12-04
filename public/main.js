1acc638a003fd6292762de1e0f5e734beddd0448
Ma Wenhe <mawenhe@Mas-MacBook-Pro.local>
2023-12-04 16:56:58 -0500

    createnewlesson

--- public/main.js
+++ public/main.js
@@ -1,76 +1,128 @@
 
 function fetchLessons() {
-    return fetch('lessons.json')
-      .then(response => response.json())
-      .catch(error => {
-        console.error('Error fetching lessons:', error);
+  return fetch('lessons.json')
+    .then(response => response.json())
+    .catch(error => {
+      console.error('Error fetching lessons:', error);
+    });
+}
+
+function fetchLessonById(id) {
+  return fetch('lessons.json')
+    .then(response => response.json())
+    .then(lessons => lessons.find(lesson => lesson.id === parseInt(id)))
+    .catch(error => {
+      console.error('Error fetching lesson:', error);
+    });
+}
+
+
+function startLesson(id) {
+  fetchLessonById(id)
+    .then(lesson => {
+      // Redirect to lesson.html with lesson details in URL parameters
+      const queryParams = new URLSearchParams({
+        id: lesson.id,
+        name: lesson.name,
+        image: lesson.image,
+        question: lesson.question
+        // Add other lesson details here if needed
       });
-  }
-  
-  function fetchLessonById(id) {
-    return fetch('lessons.json') 
-      .then(response => response.json())
-      .then(lessons => lessons.find(lesson => lesson.id === parseInt(id)))
-      .catch(error => {
-        console.error('Error fetching lesson:', error);
-      });
-  }
-  
-  
-  function startLesson(id) {
-    fetchLessonById(id)
-      .then(lesson => {
-        // Redirect to lesson.html with lesson details in URL parameters
-        const queryParams = new URLSearchParams({
-          id: lesson.id,
-          name: lesson.name,
-          image: lesson.image,
-          question: lesson.question
-          // Add other lesson details here if needed
-        });
-        window.location.href = `lesson.html?${queryParams.toString()}`;
-      })
-      .catch(error => {
-        console.error('Error fetching lesson:', error);
-      });
-  }
-  
-  function showLessons() {
-    fetchLessons()
-      .then(lessons => {
-        const container = document.getElementById('lessons-container');
-        container.innerHTML = '';
-  
-        lessons.forEach(lesson => {
-          const divLesson = document.createElement('div');
-          divLesson.className = 'lesson';
-  
-          const img = document.createElement('img');
-          img.className = 'image';
-          img.src = lesson.image ? lesson.image : '1989.jpg';
-  
-          const divName = document.createElement('div');
-          divName.className = 'name';
-          const h2 = document.createElement('h2');
-          h2.textContent = lesson.name;
-  
-          const startButton = document.createElement('button');
-          startButton.className = 'start-button';
-          startButton.textContent = 'Start';
-          startButton.addEventListener('click', () => {
-            startLesson(lesson.id); // Start the lesson by ID
-          });
-  
-          divName.appendChild(h2);
-          divLesson.appendChild(img);
-          divLesson.appendChild(divName);
-          divLesson.appendChild(startButton);
-  
-          container.appendChild(divLesson);
-        });
-      })
-      .catch(error => {
-        console.error('Error displaying lessons:', error);
-      });
-  }  
-  
+      window.location.href = `lesson.html?${queryParams.toString()}`;
+    })
+    .catch(error => {
+      console.error('Error fetching lesson:', error);
+    });
+}
+function addLessonToPage(lesson) {
+  const container = document.getElementById('lessons-container');
+  const divLesson = document.createElement('div');
+  divLesson.className = 'lesson';
+
+  const img = document.createElement('img');
+  img.className = 'image';
+  img.src = lesson.image ? lesson.image : '1989.jpg'; // Use a placeholder image or update as needed
+
+  const divName = document.createElement('div');
+  divName.className = 'name';
+  const h2 = document.createElement('h2');
+  h2.textContent = lesson.name;
+
+  const startButton = document.createElement('button');
+  startButton.className = 'start-button';
+  startButton.textContent = 'Start';
+  startButton.addEventListener('click', () => {
+    startLesson(lesson.id); // Start the lesson by ID
+  });
+
+  divName.appendChild(h2);
+  divLesson.appendChild(img);
+  divLesson.appendChild(divName);
+  divLesson.appendChild(startButton);
+
+  container.appendChild(divLesson);
+}
+
+function createNewLesson(event) {
+  event.preventDefault(); // Prevent the form from submitting in the traditional way
+
+  var lessonName = document.getElementById('lesson-name').value;
+  var lessonQuestion = document.getElementById('lesson-question').value;
+
+  var newLesson = {
+    id: Date.now(),
+    name: lessonName,
+    image: '1989.jpg', // You might want to update this
+    question: lessonQuestion
+  };
+
+  fetch('/lessons', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(newLesson),
+  })
+    .then(response => response.json())
+    .then(data => {
+      console.log('Lesson created:', data);
+      addLessonToPage(newLesson); // Add lesson to page after confirmation from server
+      window.location.href = '/educator.html';
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+    });
+
+
+  //addLessonToPage(newLesson);
+
+}
+
+function showLessons() {
+  fetchLessons()
+    .then(lessons => {
+      const container = document.getElementById('lessons-container');
+      container.innerHTML = '';
+      lessons.forEach(addLessonToPage);
+    })
+    .catch(error => {
+      console.error('Error displaying lessons:', error);
+    });
+}
+
+function showCreateLessonForm() {
+  var formHtml = `
+      <form id="create-lesson-form">
+          <label for="lesson-name">Lesson Name:</label>
+          <input type="text" id="lesson-name" name="lesson-name" required><br><br>
+
+          <label for="lesson-question">Lesson Question:</label>
+          <input type="text" id="lesson-question" name="lesson-question" required><br><br>
+
+          <button type="submit">Submit</button>
+      </form>
+  `;
+
+  document.getElementById('lessons-container').innerHTML = formHtml;
+  document.getElementById('create-lesson-form').onsubmit = createNewLesson;
+}
--- server.js
+++ server.js
@@ -2,13 +2,35 @@ const express = require('express');
 const app = express();
 const lessons = require('./public/lessons.json');
 const path = require('path');
+const fs = require('fs');
 
+// Middleware to parse JSON bodies
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
-
+// GET endpoint for lessons
 app.get('/lessons', (req, res) => {
   res.json(lessons);
 });
 
+// POST endpoint to add a new lesson
+app.post('/lessons', (req, res) => {
+  const newLesson = req.body;
+  fs.readFile('./public/lessons.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send('Error reading lessons data');
+      return;
+    }
+    const lessons = JSON.parse(data);
+    lessons.push(newLesson);
+    fs.writeFile('./public/lessons.json', JSON.stringify(lessons, null, 2), 'utf8', (err) => {
+      if (err) {
+        res.status(500).send('Error updating lessons data');
+        return;
+      }
+      res.status(201).send('Lesson added successfully');
+    });
+  });
+});
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
